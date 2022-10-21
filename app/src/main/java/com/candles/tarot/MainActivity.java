e2449f090207ea67dd9afdf03ce579696ef84204
Hoang <lykhoa2112@gmail.com>
2022-10-21 09:58:39 +0700

    1

---
+++ app/src/main/java/com/candles/tarot/MainActivity.java
@@ -0,0 +1,45 @@
+package com.candles.tarot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_join, btn_exit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvent();
+    }
+
+    private void addEvent() {
+        //-------------button exit-------------//
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(1);
+            }
+        });
+        //-------------button join-------------//
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Đã vào tarot ! hãy thử rút một lá bài.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void addControls() {
+        btn_join = findViewById(R.id.btn_join);
+        btn_exit = findViewById(R.id.btn_exit);
+    }
+}
